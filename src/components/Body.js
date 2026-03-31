3467010f709f34360e97cc3a6c0cf034399cb476
Kartik Sundriyal <kartiksundriyal2004@gmail.com>
2026-03-31 15:41:14 +0530

    CustomHook store the hooks used and can be reused

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,29 +1,8 @@
 import { useEffect, useState } from "react";
-
+import useFetch from "./useFetch";
 export default function Body() {
-  const [profiles, setProfiles] = useState([]);
-//  state for generating no. of profiles given by user
-  const [numberofProfiles,setNumberofProfiles]=useState("");
-// state for finding a user
-   const [findUser,setFindUser]=useState([]);
-
-  async function generateProfiles(count) {
-    const ran = Math.floor(Math.random() * 1000); 
-    const response = await fetch(
-      `https://api.github.com/users?since=${ran}&per_page=${count}`
-    );
-    const data = await response.json();
-    setProfiles(data);
-  }
-  async function generateUser(userName){
-    const response=await fetch(`https://api.github.com/users/${userName}`);
-    const data=await response.json();
-    setProfiles([data]);
-  }
-//  for showing 10 profiles initially
-  useEffect(() => {
-    generateProfiles(10);
-  }, []);
+    // custom hook for code reusability
+    const {numberofProfiles,setNumberofProfiles,findUser,setFindUser,profiles,generateProfiles,generateUser}=useFetch();
 
   return (
     <div className="container">
---
+++ src/components/useFetch.js
@@ -0,0 +1,29 @@
+import { useState,useEffect } from "react";
+
+export default function UseFetch(){
+    const [profiles, setProfiles] = useState([]);
+//  state for generating no. of profiles given by user
+  const [numberofProfiles,setNumberofProfiles]=useState("");
+// state for finding a user
+   const [findUser,setFindUser]=useState([]);
+
+  async function generateProfiles(count) {
+    const ran = Math.floor(Math.random() * 1000); 
+    const response = await fetch(
+      `https://api.github.com/users?since=${ran}&per_page=${count}`
+    );
+    const data = await response.json();
+    setProfiles(data);
+  }
+  async function generateUser(userName){
+    const response=await fetch(`https://api.github.com/users/${userName}`);
+    const data=await response.json();
+    setProfiles([data]);
+  }
+//  for showing 10 profiles initially
+  useEffect(() => {
+    generateProfiles(10);
+  }, []);
+
+  return {numberofProfiles,setNumberofProfiles,findUser,setFindUser,profiles,generateProfiles,generateUser};
+}
