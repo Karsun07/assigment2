a2730f7eb888fa62e3352bc7815d4c82fa34cb2d
Kartik Sundriyal <kartiksundriyal2004@gmail.com>
2026-03-31 11:23:27 +0530

    first

---
+++ src/Main.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+function App(){
+    return(
+        <>
+        
+        </>
+    )
+
+}
+const reactRoot=ReactDOM.createRoot(document.getElementById("root"));
+reactRoot.render(<App/>);
